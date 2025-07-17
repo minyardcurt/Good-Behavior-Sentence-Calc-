e15177583e74b59fb850fc131855a5102e0f8516
CurtMinyard <cminyard@perseverenow.org>
2025-07-17 08:26:08 -0500

    Seventh Commit

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function calculateRealGoodTime() {
   const goodBehaviorDaysVal = document.getElementById('goodBehaviorDays').value;
   const result = document.getElementById('result');
   
+  
   const goodBehaviorDays = parseInt(goodBehaviorDaysVal, 10);
   if (isNaN(goodBehaviorDays) || goodBehaviorDays <= 0) {
     result.textContent = "Please enter a valid number for Good Behavior Days.";
